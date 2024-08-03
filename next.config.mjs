ea3cc1d483e0e16364adb91698413458ea8504eb
Satyam Dwivedi <122256218+satyam7579@users.noreply.github.com>
2024-08-03 16:35:54 +0530

    next image used

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+  images: {
+    domains: ["cdn.myanimelist.net"],
+  },
+};
 export default nextConfig;
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,7 +1,15 @@
-export default function Card(props:any){
-    return(
-        <div className="h-[20rem] overflow-hidden p-2">
-            <img src={props.img} alt="" className="h-[20rem] w-[14rem]"/>
-        </div>
-    )
+import Image from "next/image";
+
+export default function Card(props: any) {
+  return (
+    <div className="w-[40rem] h-[25rem] bg-white">
+      <Image
+        src={props.img}
+        alt="anime poster"
+        height={0}
+        width={500}
+        className="mx-4"
+      ></Image>
+    </div>
+  );
 }
