3542a52d6d788242b652309e7af830df3a1ff3c6
ShivamSultaniya <142833938+ShivamSultaniya@users.noreply.github.com>
2024-08-03 19:13:04 +0530

    Overplay

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,7 +21,7 @@ async function Home() {
   return (
     <>
       <main className="flex flex-col w-[100vw] overflow-y-scroll">
-        <div className="flex overflow-y-auto w-[250vw] m-[4rem] bg-black">
+        <div className="flex overflow-y-auto w-[275vw] m-[6rem] bg-black gap-6">
           {result.data.map((anime: Anime) => (
             <Card key={anime.mal_id} img={anime.images.webp.large_image_url} name={anime.title_english}/>
           ))}
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,13 +1,13 @@
 export default function Card(props: any) {
   return (
-    <div className="cardContainer">
+    <div className="cardContainer w-[20rem] h-[28rem]">
 
-      <div className="hidden w-[15rem] h-[28rem] bg-gray-700 cardOverlay">
+      <div className="animeInfo bg-gray-950 h-full w-full p-4">
         <p className="text-white text-md">{props.name}</p>
       </div>
 
-      <div className="w-[15rem] h-[28rem] bg-black pr-4 flex flex-col gap-2 card">
-        <img src={props.img} className="w-[15rem] h-[22.5rem]"></img>
+      <div className="bg-black flex flex-col gap-2 card h-full overflow-hidden">
+        <img src={props.img} className="w-[18rem] h-[23rem]"></img>
         <p className="text-white text-md">{props.name}</p>
         <p className="text-gray-500 text-sm">Sub | Dub</p>
       </div>
