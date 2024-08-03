bc3c620e0ce0c49b1f4d03f89adfed1681c1f46d
ShivamSultaniya <142833938+ShivamSultaniya@users.noreply.github.com>
2024-08-03 17:55:18 +0530

    Overlay Creation

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,9 +1,16 @@
 export default function Card(props: any) {
   return (
-    <div className="w-[15rem] h-[28rem] bg-black pr-4 flex flex-col gap-2">
-      <img src={props.img} className="w-[15rem] h-[22.5rem]"></img>
-      <p className="text-white">{props.name}</p>
-      <p className="text-gray-500 text-sm">Sub | Dub</p>
+    <div className="cardContainer">
+
+      <div className="hidden w-[15rem] h-[28rem] bg-gray-700 cardOverlay">
+        <p className="text-white text-md">{props.name}</p>
+      </div>
+
+      <div className="w-[15rem] h-[28rem] bg-black pr-4 flex flex-col gap-2 card">
+        <img src={props.img} className="w-[15rem] h-[22.5rem]"></img>
+        <p className="text-white text-md">{props.name}</p>
+        <p className="text-gray-500 text-sm">Sub | Dub</p>
+      </div>
     </div>
   );
 }
