b1c209f06a3dd000d1d83d6724bcb261d419d451
Satyam Dwivedi <122256218+satyam7579@users.noreply.github.com>
2024-08-03 16:29:21 +0530

    navbar added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,18 @@
 import { fetchAnime } from "./action";
-import Card from "@/components/Card"
+import Card from "@/components/Card";
 async function Home() {
   const result = await fetchAnime();
   console.log(result);
 
   return (
     <>
-    <h1>CrunchyRoll</h1>
-    <main className="flex flex-col w-[100vw] overflow-y-scroll">
-      <div className="flex overflow-y-auto w-[200vw]">
-        {result.data.map((anime: any) => (
-          // <div key={anime.mal_id} className="w-[35rem]">
-          //   <p>{anime.mal_id}</p>
-          //   <img src={anime.images.webp.large_image_url} alt="anime logo" className="w-full h-80"/>
-          // </div>
-          <Card key={anime.mal_id}  img={anime.images.webp.large_image_url}/>
-        ))}
-      </div>
-      
-    </main>
+      <main className="flex flex-col w-[100vw] overflow-y-scroll">
+        <div className="flex overflow-y-auto w-[200vw]">
+          {result.data.map((anime: any) => (
+            <Card key={anime.mal_id} img={anime.images.webp.large_image_url} />
+          ))}
+        </div>
+      </main>
     </>
   );
 }
