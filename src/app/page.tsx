fc6c9b74c7377e76c85e051b4a912f2e12af3dd8
ShivamSultaniya <142833938+ShivamSultaniya@users.noreply.github.com>
2024-08-03 17:18:52 +0530

    Card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,19 @@
 import { fetchAnime } from "./action";
 import Card from "@/components/Card";
+
+interface Anime {
+  mal_id: number;
+  title: string;
+  synopsis: string;
+  url: string;
+  images: {
+    webp: {
+      large_image_url: string;
+    };
+  };
+  title_english: string;
+}
+
 async function Home() {
   const result = await fetchAnime();
   console.log(result);
@@ -7,9 +21,9 @@ async function Home() {
   return (
     <>
       <main className="flex flex-col w-[100vw] overflow-y-scroll">
-        <div className="flex overflow-y-auto w-[200vw]">
-          {result.data.map((anime: any) => (
-            <Card key={anime.mal_id} img={anime.images.webp.large_image_url} />
+        <div className="flex overflow-y-auto w-[250vw] m-[4rem] bg-black">
+          {result.data.map((anime: Anime) => (
+            <Card key={anime.mal_id} img={anime.images.webp.large_image_url} name={anime.title_english}/>
           ))}
         </div>
       </main>
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,15 +1,9 @@
-import Image from "next/image";
-
 export default function Card(props: any) {
   return (
-    <div className="w-[40rem] h-[25rem] bg-white">
-      <Image
-        src={props.img}
-        alt="anime poster"
-        height={0}
-        width={500}
-        className="mx-4"
-      ></Image>
+    <div className="w-[15rem] h-[28rem] bg-black pr-4 flex flex-col gap-2">
+      <img src={props.img} className="w-[15rem] h-[22.5rem]"></img>
+      <p className="text-white">{props.name}</p>
+      <p className="text-gray-500 text-sm">Sub | Dub</p>
     </div>
   );
 }
