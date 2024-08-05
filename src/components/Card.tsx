d35e359a6b458747c0c5e5ac57be376d274b35e5
Aman Deol <amandeolm@gmail.com>
2024-08-05 22:51:35 +0530

    test2

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -9,15 +9,13 @@ export default function Card(props: any) {
 
       <div className="bg-black flex flex-col gap-2 card h-full overflow-hidden">
         <img src={props.img} className="w-[18rem] h-[23rem]"></img>
-<<<<<<< HEAD
+
         <p className="text-white text-md">{props.name}</p>
       
         <p className="text-gray-500 text-sm">Sub | Dub</p>
 
-=======
-        <p className="text-white text-md font-light">{props.name}</p>
-        <p className="text-gray-500 text-sm font-normal">Sub | Dub</p>
->>>>>>> de0b5a7d06ac50dcaef42fc355c39808222df46c
+
+
       </div>
     </div>
   );
