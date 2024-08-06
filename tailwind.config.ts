ec57cace61983fa369dc9e70b6a5121d20331498
ShivamSultaniya <142833938+ShivamSultaniya@users.noreply.github.com>
2024-08-06 11:35:29 +0530

    Card hover overlay added

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,9 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      backgroundColor: {
+        'black-overlay': 'rgba(19,20,25,0.95)',
+      },
     },
   },
   plugins: [
