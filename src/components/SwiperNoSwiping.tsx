ee58146113ce505fb7de9964c178b7967ce9a244
ShivamSultaniya <142833938+ShivamSultaniya@users.noreply.github.com>
2024-08-06 20:16:09 +0530

    Fade in fade out of banner page

--- src/components/SwiperNoSwiping.tsx
+++ src/components/SwiperNoSwiping.tsx
@@ -1,30 +1,37 @@
 "use client"
-import { useState } from "react"
-import { FaRegBookmark } from "react-icons/fa6"
-import { LuPlay } from "react-icons/lu"
-import { landingPageApi } from "@/interfaces/landingPage"
+import { useState, useEffect } from "react";
+import { FaRegBookmark } from "react-icons/fa6";
+import { LuPlay } from "react-icons/lu";
+import { landingPageApi } from "@/interfaces/landingPage";
 import { MdKeyboardArrowRight } from "react-icons/md";
 
-
 export default function SwiperNoSwiping() {
-
-    const [index, setIndex] = useState(0)
+    const [index, setIndex] = useState(0);
+    const [fade, setFade] = useState(true);
 
     function handleDecrement() {
-        if (index == 0) {
-            setIndex(4);
-        }
-        else {
-            setIndex(index - 1);
-        }
+        setFade(false);
+        setTimeout(() => {
+            setIndex((index === 0 ? 4 : index - 1));
+            setFade(true);
+        }, 200);
+    }
+
+    function handleIncrement() {
+        setFade(false);
+        setTimeout(() => {
+            setIndex((index + 1) % 5);
+            setFade(true);
+        }, 200);
     }
 
     return (
         <>
-            <div className="relative overflow-hidden">
+            <div className={`relative overflow-hidden transition-opacity duration-500 ${fade ? 'opacity-100' : 'opacity-0'}`}>
+
                 <img src={landingPageApi[index].heroImage} alt="" className="h-[105vh] z-[5] w-[100vw] object-cover" />
                 <div onClick={() => handleDecrement()} className="absolute top-[10rem] z-[9] left-0 w-4 h-4">
-                    <MdKeyboardArrowRight className="text-white text-5xl rotate-180 z-[9] absolute top-[25vh] hover:text-gray-500 transition-colors cursor-pointer"/>
+                    <MdKeyboardArrowRight className="text-white text-5xl rotate-180 z-[9] absolute top-[25vh] hover:text-gray-500 transition-colors cursor-pointer" />
                 </div>
                 <img src={landingPageApi[index].logoImage} alt="" className="w-[20rem] z-[5] pb-4 ml-[3.25rem] scale-75 absolute bottom-[29rem] left-0" />
                 <p className="text-gray-500 text-sm font-normal pb-2 ml-[3.25rem] absolute top-[22.5rem] left-4 z-[5]">Sub | Dub</p>
@@ -41,12 +48,12 @@ export default function SwiperNoSwiping() {
                         &nbsp; Add to Watchlist
                     </button>
                 </div>
-                <div onClick={() => setIndex((index + 1) % 5)} className="absolute top-[10rem] z-[9] left-0 w-4 h-4">
-                    <MdKeyboardArrowRight className="text-white text-5xl absolute top-[25vh] z-[9] left-[97vw] hover:text-gray-500 transition-colors cursor-pointer"/>
+                <div onClick={() => handleIncrement()} className="absolute top-[10rem] z-[9] left-0 w-4 h-4">
+                    <MdKeyboardArrowRight className="text-white text-5xl absolute top-[25vh] z-[9] left-[97vw] hover:text-gray-500 transition-colors cursor-pointer" />
                 </div>
                 <div className="absolute top-[0rem] left-[-10rem] z-[4] w-[140vw] h-[160vh] verticalfade"></div>
                 <div className="absolute top-[0rem] left-[-10rem] z-[4] w-[140vw] h-[160vh] horizontalfade"></div>
             </div>
         </>
-    )
+    );
 }
