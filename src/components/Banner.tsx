a804d0302be222abf76805193d643edb465414c0
Satyam Dwivedi <122256218+satyam7579@users.noreply.github.com>
2024-08-06 11:37:50 +0530

    Banner added

---
+++ src/components/Banner.tsx
@@ -0,0 +1,19 @@
+import { Anime } from "@/interfaces/Anime";
+interface BannerProps{
+    apicall: Anime;
+}
+const Banner: React.FC<BannerProps> = ({apicall}) => {
+  return (
+    <section className="w-[80vw] h-[30rem] mx-auto my-12">
+      {apicall.trailer?.images?.maximum_image_url && (
+        <img
+          src={apicall.trailer.images.maximum_image_url}
+          alt={apicall.title}
+          className="w-full h-full"
+          loading="lazy"
+        />
+      )}
+    </section>
+  );
+}
+export default Banner;
