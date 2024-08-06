213cf267a9ce6984098db5c4496716ede38fa5e8
ShivamSultaniya <142833938+ShivamSultaniya@users.noreply.github.com>
2024-08-06 17:36:22 +0530

    Landing Page info added

--- src/components/SwiperNoSwiping.tsx
+++ src/components/SwiperNoSwiping.tsx
@@ -6,14 +6,25 @@ import { landingPageApi } from "@/interfaces/landingPage"
 
 export default function SwiperNoSwiping() {
 
-    const [index, setIndex] = useState(1)
+    const [index, setIndex] = useState(0)
+
+    function handleDecrement() {
+        if(index == 0){
+            setIndex(4);
+        }
+        else{
+            setIndex(index-1);
+        }
+    }
 
     return (
         <>
             <div className="relative overflow-hidden">
                 <img src={landingPageApi[index].heroImage} alt="" className="h-[105vh] w-[100vw] object-cover"/>
                 <div className="absolute top-[11rem] left-0 z-[9]">
-                    <img src="arrow.png" alt="" className="ml-4 rotate-180 pre-button absolute top-[50%]"/>
+                    <div onClick={() => handleDecrement()}>
+                        <img src="arrow.png" alt="" className="ml-4 rotate-180 pre-button absolute top-[25vh]"/>
+                    </div>
                     <img src={landingPageApi[index].logoImage} alt="" className="w-[20rem] pb-4 ml-[3.25rem]" />
                     <p className="text-gray-500 text-sm font-normal pb-2 ml-[3.25rem]">Sub | Dub</p>
                     <p className="text-white text-md font-normal w-[27rem] pb-6 ml-[3.25rem]">{landingPageApi[index].description}</p>
@@ -29,7 +40,9 @@ export default function SwiperNoSwiping() {
                             &nbsp; Add to Watchlist
                         </button>
                     </div>
-                    <img src="arrow.png" alt="" className="ml-4 pre-button absolute top-[50%] left-[46vw]"/>
+                    <div onClick={() => setIndex((index+1)%5)}>
+                        <img src="arrow.png" alt="" className="ml-4 pre-button absolute top-[25vh] left-[97vw]"/>
+                    </div>
                 </div>
                 <div className="absolute top-[0rem] left-[-10rem] z-[8] w-[140vw] h-[160vh] fade">
                 </div>
