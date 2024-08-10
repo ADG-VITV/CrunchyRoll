2d129df4966042b41589d0ae8cc266ae606aebf7
Satyam Dwivedi <122256218+satyam7579@users.noreply.github.com>
2024-08-10 15:22:16 +0530

    debugging

--- src/app/action.ts
+++ src/app/action.ts
@@ -1,18 +1,12 @@
 export const fetchAnime = async(url: string) => {
     const response = await fetch(url)
     const data = await response.json()
-    // const animeList = data.data;
-    // return animeList;
-    if(data) return data.data;
-    return [];
+    const animeList = data.data;
+    return animeList;
 }
 export const reverseFetchAnime = async(url: string) => {
     const response = await fetch(url)
     const data = await response.json()
-    // const animeList = data.data;
-    // console.log("----------------------------------------------------------");
-    // console.log(animeList);
-    // return animeList.reverse() || [];
-    if(data) return data.data.reverse();
-    return [];
+    const animeList = data.data;
+    return animeList;
 }
